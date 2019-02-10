a76701e687063d62506b9c4fb474e94c133d6642
Mr Robot <gherb95@gmail.com>
2019-02-10 18:55:39 +0100

    LogService is ultimate

--- src/java/model/session/dao/CookieImpl/CookieSessionDAOFactory.java
+++ src/java/model/session/dao/CookieImpl/CookieSessionDAOFactory.java
@@ -5,10 +5,41 @@
  */
 package model.session.dao.CookieImpl;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.session.dao.LoggedUserDAO;
+import model.session.dao.SessionDAOFactory;
 /**
  *
  * @author Mr. Robot
  */
+
+/**
+ * 
+ * classe che implementa i cookie 
+ * @param request Richiesta dati http
+ * @param response risposta al client
+ */
+
 public class CookieSessionDAOFactory {
-    
+    private HttpServletRequest request; //variabili d'esemplare della classe 
+    private HttpServletResponse response;
+
+    @Override
+    public void initSession(HttpServletRequest request, HttpServletResponse response) {
+
+        try {
+          this.request=request;
+          this.response=response;
+        } catch (Exception e) {
+          throw new RuntimeException(e);
+          }
+
+    }
+
+    @Override
+    public LoggedUserDAO getLoggedUserDAO() {
+        return new LoggedUserDAOCookieImpl(request,response);
+    }
 }
