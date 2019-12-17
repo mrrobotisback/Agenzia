95c2430bd687cca5d84daec4edd9ec41f4428791
Nicolo.garbin <nicolo.garbin@triboo.it>
2019-12-17 21:50:07 +0100

    Implement Confiuration for db connection

--- src/java/services/config/Configuration.java
+++ src/java/services/config/Configuration.java
@@ -1,14 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package services.config;
 
-/**
- *
- * @author Mr. Robot
- */
+import java.util.logging.Level;
+
+import model.dao.DAOFactory;
+import model.session.dao.SessionDAOFactory;
+
 public class Configuration {
-    
+
+    /* Database Configruation */
+    public static final String DAO_IMPL=DAOFactory.MYSQLJDBCIMPL;
+    public static final String DATABASE_DRIVER="com.mysql.jdbc.Driver";
+    public static final String DATABASE_URL="jdbc:mysql://localhost/agenziaViaggi?user=phpmyadmin&password=root";
+
+    /* Session Configuration */
+    public static final String SESSION_IMPL=SessionDAOFactory.COOKIEIMPL;
+
+    /* Logger Configuration */
+    public static final String GLOBAL_LOGGER_NAME="agenzia";
+    public static final String GLOBAL_LOGGER_FILE="\\home\\nicolo\\";
+    public static final Level GLOBAL_LOGGER_LEVEL=Level.ALL;
+
 }
