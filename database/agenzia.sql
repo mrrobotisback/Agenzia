bf10a65470c717fddee2de0be0dddd9c16fec8fb
Nicolo.garbin <nicolo.garbin@triboo.it>
2020-05-19 13:13:00 +0200

    Final sql script

--- database/agenzia.sql
+++ database/agenzia.sql
@@ -1,12 +1,12 @@
 # lulrulez
 # Script for database agenzia
 
-DROP DATABASE IF EXISTS agenzia;
-CREATE DATABASE IF NOT EXISTS agenzia DEFAULT CHARACTER SET utf8;
-SHOW CREATE DATABASE agenzia;
-USE agenzia;
+DROP database if exists agenzia;
+create database if not exists agenzia default character set utf8;
+show create database agenzia;
+use agenzia;
 
-DROP TABLE IF EXISTS invoice,
+drop table if exists payment,
                      `order`,
                      details,
                      cart,
@@ -18,191 +18,178 @@ DROP TABLE IF EXISTS invoice,
                      travel,
                      user;
 
--- SHOW ENGINE InnoDB STATUS;
+show engine InnoDB status;
 
-CREATE TABLE user (
-  userid int unsigned NOT NULL AUTO_INCREMENT,
-  username varchar(255) NOT NULL,
-  firstname varchar(255) NOT NULL,
-  surname varchar(255) NOT NULL,
-  password varchar(512) NOT NULL,
-  email varchar(255) NOT NULL,
-  nationality varchar(255) default NULL,
-  date_birth date NOT NULL,
-  sex char NOT NULL,
-  cellular bigint NOT NULL,
-  street varchar(255) NOT NULL,
-  number varchar(255) NOT NULL,
-  city varchar(255) NOT NULL,
-  province varchar(255) NOT NULL,
-  cap int NOT NULL,
-  avatar longtext default NULL,
-  profession varchar(255) NOT NULL,
-  cf varchar(16) NOT NULL,
-  role varchar(255) NOT NULL,
-  PRIMARY KEY (userid)
-) ENGINE=InnoDB CHARACTER SET utf8;
+create table user (
+  userid int unsigned not null auto_increment,
+  username varchar(255) not null,
+  firstname varchar(255) not null,
+  surname varchar(255) not null,
+  password varchar(512) not null,
+  email varchar(255) not null,
+  nationality varchar(255) default null,
+  date_birth date not null,
+  sex char not null,
+  cellular bigint not null,
+  street varchar(255) not null,
+  number varchar(255) not null,
+  city varchar(255) not null,
+  province varchar(255) not null,
+  cap int not null,
+  avatar longtext default null,
+  profession varchar(255) not null,
+  cf varchar(16) not null,
+  role varchar(255) not null,
+  primary key (userid)
+) engine=InnoDB character set utf8 collate utf8_general_ci;
 
-SHOW CREATE TABLE user;
+show create table user;
 
-CREATE TABLE category (
-  id int unsigned NOT NULL AUTO_INCREMENT,
-  name varchar (255) NOT NULL,
-  description varchar(255) NOT NULL,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
+create table category (
+  id int unsigned not null auto_increment,
+  name varchar (255) not null,
+  description varchar(255) not null,
+  primary key (id)
+) engine=InnoDB character set utf8 collate utf8_general_ci;
 
-SHOW CREATE TABLE category;
+show create table category;
 
-CREATE TABLE card (
-  number bigint NOT NULL,
-  holder varchar(255) NOT NULL,
-  cvv int NOT NULL,
-  expiration date NOT NULL,
-  userid int unsigned NOT NULL,
-  PRIMARY KEY (number),
-  INDEX (userid),
-  CONSTRAINT user_fk_card foreign key (userid) REFERENCES user(userid) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
-
-SHOW CREATE TABLE `card`;
-
-CREATE TABLE `order` (
-  `number` int unsigned not null AUTO_INCREMENT,
-  `travel_code` int unsigned not null,
-  `userid` int  unsigned NOT NULL,
-  `name` varchar(255) not null,
-  `date` date not null,
-  `total` double not null,
-  `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`number`),
-  INDEX (`userid`),
-  CONSTRAINT `user_fk_order` foreign key (`userid`) REFERENCES `user`(`userid`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
-
-SHOW CREATE TABLE `order`;
-
-CREATE TABLE travel (
-  id int unsigned NOT NULL AUTO_INCREMENT,
-  category_id int unsigned NOT NULL,
-  order_id int unsigned NOT NULL,
-  price double NOT NULL,
-  name varchar(255) NOT NULL,
-  discount double default null,
-  start_date date NOT NULL,
-  means bigint NOT NULL,
-  description text NOT NULL,
-  start_place varchar(255) NOT NULL,
-  start_hour time NOT NULL,
-  duration varchar(255) NOT NULL,
-  seats_available int NOT NULL,
-  seats_total int NOT NULL,
-  destination varchar(255) NOT NULL,
-  PRIMARY KEY (id),
-  INDEX (category_id),
-  INDEX (order_id),
-  CONSTRAINT category_fk_travel foreign key (category_id) REFERENCES category(id) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT order_fk_travel foreign key (order_id) REFERENCES `order`(`number`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
-
-SHOW CREATE TABLE travel;
-
-CREATE TABLE `add` (
+create table card (
+  number bigint not null,
+  holder varchar(255) not null,
+  cvv int not null,
+  expiration date not null,
   userid int unsigned not null,
+  primary key (number),
+  index (userid),
+  constraint user_fk_card foreign key (userid) references user(userid) on delete cascade on update cascade
+) engine=InnoDB character set utf8 collate utf8_general_ci;
+
+show create table `card`;
+
+create table `order` (
+  number int unsigned not null auto_increment,
   travel_code int unsigned not null,
-  PRIMARY KEY (userid, travel_code),
-  INDEX (userid),
-  INDEX (travel_code),
-  CONSTRAINT user_fk_add foreign key (userid) REFERENCES user(userid) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT travel_fk_add foreign key (travel_code) REFERENCES travel(id) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
-
-SHOW CREATE TABLE `add`;
-
-CREATE TABLE invoice (
-  number int unsigned not null,
-  name varchar(255) not null,
+  userid int  unsigned not null,
   date date not null,
   total double not null,
-  firstname varchar(255) NOT NULL,
-  lastname varchar(255) NOT NULL,
-  phone bigint NOT NULL,
-  `street` varchar(255) NOT NULL,
-  `nationality` varchar(255) NOT NULL,
-  `city` varchar(255) NOT NULL,
-  `province` varchar(255) NOT NULL,
-  `cf` varchar(16) NOT NULL,
-  `cap` int NOT NULL,
-  PRIMARY KEY (`number`),
-  CONSTRAINT `order_fk_invoice` foreign key (`number`) REFERENCES `order`(`number`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
+  created timestamp not null default current_timestamp,
+  updated timestamp not null default current_timestamp on update current_timestamp,
+  primary key (number),
+  index (userid),
+  constraint user_fk_order foreign key (userid) references user(userid) on delete cascade on update cascade
+) engine=InnoDB character set utf8 collate utf8_general_ci;
 
-SHOW CREATE TABLE invoice;
+show create table `order`;
 
-CREATE TABLE details (
+create table travel (
+  id int unsigned not null AUTO_INCREMENT,
+  category_id int unsigned not null,
+  order_id int unsigned not null,
+  price double not null,
+  name varchar(255) not null,
+  discount double default null,
+  start_date date not null,
+  means bigint not null,
+  description text not null,
+  start_place varchar(255) not null,
+  start_hour time not null,
+  duration varchar(255) not null,
+  seats_available int not null,
+  seats_total int not null,
+  destination varchar(255) not null,
+  primary key (id),
+  index (category_id),
+  index (order_id),
+  constraint category_fk_travel foreign key (category_id) references category(id) on delete cascade on update cascade,
+  constraint order_fk_travel foreign key (order_id) references `order`(`number`) on delete cascade on update cascade
+) engine=InnoDB character set utf8 collate utf8_general_ci;
+
+show create table travel;
+
+create table cart (
+  userid int unsigned not null,
+  total double default null,
+  primary key (userid),
+  constraint user_fk_cart foreign key (userid) references user(userid) on delete cascade on update cascade
+) engine=InnoDB character set utf8 collate utf8_general_ci;
+
+show create table cart;
+
+create table have (
+  userid int unsigned not null,
+  travel_code int unsigned not null,
+  quantity int default null,
+  primary key (userid, travel_code),
+  index (userid),
+  index (travel_code),
+  constraint user_fk_have foreign key (userid) references cart(userid) on delete cascade on update cascade,
+  constraint travel_fk_have foreign key (travel_code) references travel(id) on delete cascade on update cascade
+) engine=InnoDB character set utf8 collate utf8_general_ci;
+
+show create table have;
+
+create table payment (
+  number int unsigned not null,
+  date date not null,
+  amount double not null,
+  with varchar(255) not null,
+  primary key (number),
+  constraint order_fk_invoice foreign key (number) references `order`(number) on delete cascade on update cascade
+) engine=InnoDB character set utf8 collate utf8_general_ci;
+
+show create table payment;
+
+create table details (
   quantity int not null,
   order_number int unsigned not null,
   travel_code int unsigned not null,
-  PRIMARY KEY (order_number, travel_code),
-  INDEX (order_number),
-  INDEX (travel_code),
-  CONSTRAINT `order_fk_details` foreign key (order_number) REFERENCES `order`(`number`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `travel_fk_details` foreign key (`travel_code`) REFERENCES `travel`(`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
+  primary key (order_number, travel_code),
+  index (order_number),
+  index (travel_code),
+  constraint order_fk_details foreign key (order_number) references `order`(number) on delete cascade on update cascade,
+  constraint travel_fk_details foreign key (travel_code) references travel(id) on delete cascade on update cascade
+) engine=InnoDB character set utf8 collate utf8_general_ci;
 
+show create table details;
 
-CREATE TABLE cart (
-  userid int unsigned NOT NULL,
-  travel_code int unsigned NOT NULL,
-  quantity int default NULL,
-  name varchar(255) default null,
-  price double default null,
-  PRIMARY KEY (userid),
-  INDEX (travel_code),
-  CONSTRAINT user_fk_cart foreign key (userid) REFERENCES user(userid) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT travel_fk_cart foreign key (travel_code) REFERENCES travel(id) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
-
-SHOW CREATE TABLE `cart`;
-
-CREATE TABLE review (
-  id int unsigned NOT NULL AUTO_INCREMENT,
+create table review (
+  id int unsigned not null auto_increment,
   travel_code int unsigned not null,
-  userid int unsigned NOT NULL,
-  star int NOT NULL,
-  date date NOT NULL,
-  description text NOT NULL,
-  approved tinyint NOT NULL DEFAULT 0,
-  PRIMARY KEY (id),
-  INDEX (userid),
-  INDEX (travel_code),
-  CONSTRAINT user_fk_review foreign key (userid) REFERENCES user(userid) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT travel_fk_review foreign key (travel_code) REFERENCES travel(id) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
+  userid int unsigned not null,
+  star int not null,
+  date date not null,
+  description text not null,
+  approved tinyint not null default 0,
+  primary key (id),
+  index (userid),
+  index (travel_code),
+  constraint user_fk_review foreign key (userid) references user(userid) on delete cascade on update cascade,
+  constraint travel_fk_review foreign key (travel_code) references travel(id) on delete cascade on update cascade
+) engine=InnoDB character set utf8 collate utf8_general_ci;
 
-SHOW CREATE TABLE review;
+show create table review;
 
-CREATE TABLE images (
-  id int unsigned NOT NULL AUTO_INCREMENT,
+create table images (
+  id int unsigned not null auto_increment,
   travel_code int unsigned not null,
   category_id int unsigned not NULL,
   review_id int unsigned not null,
-  name varchar(255) NOT NULL,
-  path longtext NOT NULL,
-  PRIMARY KEY (id),
-  INDEX (travel_code),
-  INDEX (category_id),
-  INDEX (review_id),
-  CONSTRAINT travel_fk_images foreign key (travel_code) REFERENCES travel(id) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT category_fk_images foreign key (category_id) REFERENCES category(id) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT review_fk_images foreign key (review_id) REFERENCES review(id) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
+  name varchar(255) not null,
+  path longtext not null,
+  primary key (id),
+  index (travel_code),
+  index (category_id),
+  index (review_id),
+  constraint travel_fk_images foreign key (travel_code) references travel(id) on delete cascade on update cascade,
+  constraint category_fk_images foreign key (category_id) references category(id) on delete cascade on update cascade,
+  constraint review_fk_images foreign key (review_id) references review(id) on delete cascade on update cascade
+) engine=InnoDB character set utf8 collate utf8_general_ci;
 
-SHOW CREATE TABLE images;
+show create table images;
 
 -- Add foreign key to order
 
-ALTER TABLE `order`
-ADD CONSTRAINT travel_fk_order
-FOREIGN KEY (travel_code) REFERENCES travel(id) ON DELETE CASCADE ON UPDATE CASCADE;
+alter table `order`
+add constraint travel_fk_order
+foreign key (travel_code) references travel(id) on delete cascade on update cascade;
